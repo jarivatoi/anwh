9a8a8268ab1a596af56bf54373cbd4ab041e4870
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-06 00:18:47 +0400

    Add staff information to rosterAuth.ts

--- src/utils/rosterAuth.ts
+++ src/utils/rosterAuth.ts
@@ -2,40 +2,43 @@ export interface AuthCode {
   code: string;
   name: string;
   title?: string;
+  employeeId?: string;
+  firstName?: string;
+  surname?: string;
 }
 
 export const authCodes: AuthCode[] = [
   // Regular Staff - ID-based codes
-  { code: 'B165', name: 'BHEKUR', title: 'MIT' },
-  { code: 'B196', name: 'BHOLLOORAM', title: 'MIT' },
-  { code: 'D28B', name: 'DHUNNY', title: 'MIT' },
-  { code: 'D07D', name: 'DOMUN', title: 'SMIT' },
-  { code: 'H301', name: 'FOKEERCHAND', title: 'MIT' },
-  { code: 'S069', name: 'GHOORAN', title: 'MIT' },
-  { code: 'H13D', name: 'HOSENBUX', title: 'MIT' },
-  { code: 'J149', name: 'JUMMUN', title: 'MIT' },
-  { code: 'M17G', name: 'MAUDHOO', title: 'MIT' },
-  { code: 'N28C', name: 'NARAYYA', title: 'MIT' },
-  { code: 'P09A', name: 'PITTEA', title: 'SMIT' },
-  { code: 'R16G', name: 'RUNGADOO', title: 'SMIT' },
-  { code: 'T16G', name: 'TEELUCK', title: 'SMIT' },
-  { code: 'V160', name: 'VEERASAWMY', title: 'SMIT' },
+  { code: 'B165', name: 'BHEKUR', title: 'MIT', employeeId: '47510', firstName: 'Yashdev', surname: 'BHEKUR' },
+  { code: 'B196', name: 'BHOLLOORAM', title: 'MIT', employeeId: '47510', firstName: 'Sawan', surname: 'BHOLLOORAM' },
+  { code: 'D28B', name: 'DHUNNY', title: 'MIT', employeeId: '30060', firstName: 'Leetarvind', surname: 'DHUNNY' },
+  { code: 'D07D', name: 'DOMUN', title: 'SMIT', employeeId: '59300', firstName: 'Shamir', surname: 'DOMUN' },
+  { code: 'H301', name: 'FOKEERCHAND', title: 'MIT', employeeId: '37185', firstName: 'Needeema', surname: 'FOKEERCHAND' },
+  { code: 'S069', name: 'GHOORAN', title: 'MIT', employeeId: '38010', firstName: 'Bibi Sharinaaz', surname: 'SAMTALLY-GHOORAN' },
+  { code: 'H13D', name: 'HOSENBUX', title: 'SMIT', employeeId: '48810', firstName: 'Zameer', surname: 'HOSENBUX' },
+  { code: 'J149', name: 'JUMMUN', title: 'MIT', employeeId: '47510', firstName: 'Bibi Nawsheen', surname: 'JUMMUN' },
+  { code: 'M17G', name: 'MAUDHOO', title: 'MIT', employeeId: '38010', firstName: 'Chandanee', surname: 'MAUDHOO' },
+  { code: 'N28C', name: 'NARAYYA', title: 'MIT', employeeId: '38010', firstName: 'Viraj', surname: 'NARAYYA' },
+  { code: 'P09A', name: 'PITTEA', title: 'SMIT', employeeId: '59300', firstName: 'Pokhiraj', surname: 'PITTEA' },
+  { code: 'R16G', name: 'RUNGADOO', title: 'SMIT', employeeId: '59300', firstName: 'Manee', surname: 'RUNGADOO' },
+  { code: 'T16G', name: 'TEELUCK', title: 'SMIT', employeeId: '59300', firstName: '', surname: 'TEELUCK' },
+  { code: 'V160', name: 'VEERASAWMY', title: 'SMIT', employeeId: '59300', firstName: 'Goindah', surname: 'VEERASAWMY' },
   
   // Radiographers (R) - 99 suffix
-  { code: 'B16R', name: 'BHEKUR(R)', title: 'MIT' },
-  { code: 'B19R', name: 'BHOLLOORAM(R)', title: 'MIT' },
-  { code: 'D28R', name: 'DHUNNY(R)', title: 'MIT' },
-  { code: 'D07R', name: 'DOMUN(R)', title: 'SMIT' },
-  { code: 'H30R', name: 'FOKEERCHAND(R)', title: 'MIT' },
-  { code: 'H13R', name: 'HOSENBUX(R)', title: 'MIT' },
-  { code: 'S06R', name: 'GHOORAN(R)', title: 'MIT' },
-  { code: 'J14R', name: 'JUMMUN(R)', title: 'MIT' },
-  { code: 'M17R', name: 'MAUDHOO(R)', title: 'MIT' },
-  { code: 'N28R', name: 'NARAYYA(R)', title: 'MIT' },
-  { code: 'P09R', name: 'PITTEA(R)', title: 'SMIT' },
-  { code: 'R21R', name: 'RUNGADOO(R)', title: 'SMIT' },
-  { code: 'T16R', name: 'TEELUCK(R)', title: 'SMIT' },
-  { code: 'V16R', name: 'VEERASAWMY(R)', title: 'SMIT' },
+  { code: 'B16R', name: 'BHEKUR(R)', title: 'MIT', employeeId: '47510', firstName: 'Yashdev', surname: 'BHEKUR' },
+  { code: 'B19R', name: 'BHOLLOORAM(R)', title: 'MIT', employeeId: '47510', firstName: 'Sawan', surname: 'BHOLLOORAM' },
+  { code: 'D28R', name: 'DHUNNY(R)', title: 'MIT', employeeId: '30060', firstName: 'Leetarvind', surname: 'DHUNNY' },
+  { code: 'D07R', name: 'DOMUN(R)', title: 'SMIT', employeeId: '59300', firstName: 'Shamir', surname: 'DOMUN' },
+  { code: 'H30R', name: 'FOKEERCHAND(R)', title: 'MIT', employeeId: '37185', firstName: 'Needeema', surname: 'FOKEERCHAND' },
+  { code: 'H13R', name: 'HOSENBUX(R)', title: 'MIT', employeeId: '48810', firstName: 'Zameer', surname: 'HOSENBUX' },
+  { code: 'S06R', name: 'GHOORAN(R)', title: 'MIT', employeeId: '38010', firstName: 'Bibi Sharinaaz', surname: 'SAMTALLY-GHOORAN' },
+  { code: 'J14R', name: 'JUMMUN(R)', title: 'MIT', employeeId: '47510', firstName: 'Bibi Nawsheen', surname: 'JUMMUN' },
+  { code: 'M17R', name: 'MAUDHOO(R)', title: 'MIT', employeeId: '38010', firstName: 'Chandanee', surname: 'MAUDHOO' },
+  { code: 'N28R', name: 'NARAYYA(R)', title: 'MIT', employeeId: '38010', firstName: 'Viraj', surname: 'NARAYYA' },
+  { code: 'P09R', name: 'PITTEA(R)', title: 'SMIT', employeeId: '59300', firstName: 'Pokhiraj', surname: 'PITTEA' },
+  { code: 'R21R', name: 'RUNGADOO(R)', title: 'SMIT', employeeId: '59300', firstName: 'Manee', surname: 'RUNGADOO' },
+  { code: 'T16R', name: 'TEELUCK(R)', title: 'SMIT', employeeId: '59300', firstName: '', surname: 'TEELUCK' },
+  { code: 'V16R', name: 'VEERASAWMY(R)', title: 'SMIT', employeeId: '59300', firstName: 'Goindah', surname: 'VEERASAWMY' },
   
   // Admin Code
   { code: '5274', name: 'ADMIN', title: 'ADMIN' }
@@ -131,4 +134,24 @@ export function validateAuthCode(code: string): string | null {
 
 export function isAdminCode(code: string): boolean {
   return code.toUpperCase() === ADMIN_CODE;
+}
+
+// Helper functions to get staff information
+export function getStaffInfo(staffName: string): AuthCode | null {
+  return authCodes.find(auth => auth.name === staffName) || null;
+}
+
+export function getStaffFullName(staffName: string): string {
+  const staffInfo = getStaffInfo(staffName);
+  if (!staffInfo) return staffName;
+  
+  const firstName = staffInfo.firstName || '';
+  const surname = staffInfo.surname || staffInfo.name;
+  
+  return firstName ? `${firstName} ${surname}` : surname;
+}
+
+export function getStaffEmployeeId(staffName: string): string {
+  const staffInfo = getStaffInfo(staffName);
+  return staffInfo?.employeeId || '';
 }
