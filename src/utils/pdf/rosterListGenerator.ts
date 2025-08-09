0075e41db3b5525b0a4880c674981cbe5bec99d6
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-09 12:48:57 +0400

    Updated rosterListGenerator.ts

--- src/utils/pdf/rosterListGenerator.ts
+++ src/utils/pdf/rosterListGenerator.ts
@@ -75,18 +75,6 @@ export class RosterListGenerator {
                 let currentX = data.cell.x + 2;
                 let currentLine = 0;
                 const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
-                const lineHeight = 3;
                 const maxWidth = data.cell.width - 4;
                 let totalLines = 1;
                 let tempX = 0;
@@ -128,7 +116,6 @@ export class RosterListGenerator {
                   if (currentX + textWidth > data.cell.x + maxWidth && index > 0) {
                    // Add comma at the end of current line if there are more names
                    if (index < staffNamesData.length - 1) {
-                    if (index < staffNamesData.length - 1) {
                       doc.text(',', currentX, cellY);
                     }
                     
@@ -185,6 +172,8 @@ export class RosterListGenerator {
         theme: 'grid',
         tableLineWidth: 0.25,
         tableLineColor: [0, 0, 0]
+      });
+    }
     
     // Footer
     doc.setFont('helvetica', 'normal');
