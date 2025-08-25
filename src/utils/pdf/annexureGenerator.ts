a9eebf25ad7c00296d7a8b22dcf33f09e4c4201b
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-26 01:32:00 +0400

    Updated annexureGenerator.ts

--- src/utils/pdf/annexureGenerator.ts
+++ src/utils/pdf/annexureGenerator.ts
@@ -1,4 +1,4 @@
-import { jsPDF } from 'jspdf';
+import { jsPDF } from 'jspdf'; 
 import autoTable from 'jspdf-autotable';
 import { RosterEntry } from '../../types/roster';
 import { formatMauritianRupees } from '../currency';
