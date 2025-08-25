d92996fa46405d3a43e3fbf51b6b5db6c0bb8fbb
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-26 00:49:57 +0400

    Updated annexureGenerator.ts

--- src/utils/pdf/annexureGenerator.ts
+++ src/utils/pdf/annexureGenerator.ts
@@ -163,7 +163,7 @@ export class AnnexureGenerator {
     doc.setFontSize(12);
     doc.text(`GRAND TOTAL AMOUNT: ${this.formatCurrency(grandTotal)}`, 15, finalY + 36);
 */
-
+doc.text('Certified correct as per annexture:', 50, finalY+200);
     
     // Footer
     doc.setFont('helvetica', 'normal');
