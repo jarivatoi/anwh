c68abd07d39be5d10a063e160143769c44e3681a
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-26 02:00:12 +0400

    Updated individualBillGenerator.ts

--- src/utils/pdf/individualBillGenerator.ts
+++ src/utils/pdf/individualBillGenerator.ts
@@ -260,8 +260,8 @@ export class IndividualBillGenerator {
     // Left side - Date and signature
     doc.setFont('helvetica', 'normal');
     doc.setFontSize(8);
-    doc.text('Date and Signature: ________________', 15, summaryEndY + 12);
-    doc.text('Signature:_________________', 15, summaryEndY + 19);
+    doc.text('Date and Signature: ________________', 15, summaryEndY + 19);
+    doc.text('Signature:_________________', 15, summaryEndY + 26);
     
     // Right side - Certification
     const rightX = 120;
