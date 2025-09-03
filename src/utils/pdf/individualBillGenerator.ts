369df23912049f86e38f0c7c3dccad441bbd4f9f
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-09-03 23:27:23 +0400

    Updated individualBillGenerator.ts

--- src/utils/pdf/individualBillGenerator.ts
+++ src/utils/pdf/individualBillGenerator.ts
@@ -297,9 +297,9 @@ export class IndividualBillGenerator {
     const rightX = 120;
     doc.setFontSize(10);
     doc.text('Certified correct as per attendance.', rightX, summaryEndY-40);
-    doc.text('Name :- ________________________________________', rightX, summaryEndY -32);
+    doc.text('Name :- ______________________________________', rightX, summaryEndY -32);
     doc.text('Grade: Principal Medical Imaging Technologist.', rightX, summaryEndY - 26);
-    doc.text('Signature:- ______________________________________', rightX, summaryEndY -18);
+    doc.text('Signature:- ___________________________________', rightX, summaryEndY -18);
   }
    
   /**
