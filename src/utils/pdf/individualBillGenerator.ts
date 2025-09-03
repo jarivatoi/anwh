56d011d5392285feeab0eb664d6e3e9e39706dae
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-09-03 23:26:41 +0400

    Updated individualBillGenerator.ts

--- src/utils/pdf/individualBillGenerator.ts
+++ src/utils/pdf/individualBillGenerator.ts
@@ -296,12 +296,12 @@ export class IndividualBillGenerator {
     // Right side - Certification
     const rightX = 120;
     doc.setFontSize(10);
-    doc.text('Certified correct as per attendance.', rightX, summaryEndY-20);
-    doc.text('Name :- ________________________________________', rightX, summaryEndY -12);
-    doc.text('Grade: Principal Medical Imaging Technologist.', rightX, summaryEndY - 4);
-    doc.text('Signature:- ______________________________________', rightX, summaryEndY );
+    doc.text('Certified correct as per attendance.', rightX, summaryEndY-40);
+    doc.text('Name :- ________________________________________', rightX, summaryEndY -32);
+    doc.text('Grade: Principal Medical Imaging Technologist.', rightX, summaryEndY - 26);
+    doc.text('Signature:- ______________________________________', rightX, summaryEndY -18);
   }
-  
+   
   /**
    * Filter roster entries for specific staff member and month
    */
